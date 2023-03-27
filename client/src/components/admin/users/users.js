9802f9f52f03098dc882f4969b2790ce1fa43ceb
hamza1022 <hamzatanveer172@gmail.com>
2023-03-27 16:32:48 +0500

    fix

--- client/src/components/admin/users/users.js
+++ client/src/components/admin/users/users.js
@@ -2,6 +2,7 @@ import React, { useState ,useEffect} from 'react'
 import { BackendApi } from '../../../api'
 import { AdminDashboard } from '../dashboard'
 import Table from 'react-bootstrap/Table';
+import Swal from 'sweetalert2'
 
 import {FaTrashAlt} from 'react-icons/fa'
 import {ImCross} from 'react-icons/im'
@@ -10,6 +11,42 @@ import {BsCheckLg} from 'react-icons/bs'
 const Users = () => {
 
     const [users,setUsers]=useState([])
+    const deleteUser = (user) => {
+      // Show confirmation dialog
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Delete user if user confirms
+          BackendApi.user.deleteUser(user)
+            .then(() => {
+              // Reload user list after successful deletion
+              fetchUsers();
+              // Show success message
+              Swal.fire(
+                'Deleted!',
+                'User has been deleted.',
+                'success'
+              )
+            })
+            .catch((err) => {
+              console.log(err)
+              // Show error message
+              Swal.fire(
+                'Error!',
+                'Failed to delete user.',
+                'error'
+              )
+            })
+        }
+      })
+    }
 
     const fetchUsers= async ()=>{
         BackendApi.user.getAllUsers({role:"user"})
@@ -26,18 +63,18 @@ const Users = () => {
 
     }
 
-    const deleteUser = (user)=>{
+    // const deleteUser = (user)=>{
 
-      BackendApi.user.deleteUser(user)
-      .then((users)=>{
-        fetchUsers()
-        console.log(users)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    //   BackendApi.user.deleteUser(user)
+    //   .then((users)=>{
+    //     fetchUsers()
+    //     console.log(users)
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
 
-    }
+    // }
 
     const changeStatus =(user, newStatus)=>{
       BackendApi.user.changeStatus(user.email,newStatus)
