31e94d79b6a3a3657bc59c494be1736887281ab9
hamza1022 <hamzatanveer172@gmail.com>
2023-03-28 15:07:28 +0500

    Paths added

--- client/src/components/layout/app-layout.js
+++ client/src/components/layout/app-layout.js
@@ -17,7 +17,7 @@ import {
 
 import MenuIcon from '@mui/icons-material/Menu';
 
-import  SignUp from "../registration/signUp"
+import SignUp from "../registration/signUp"
 import { Sidebar } from "./sidebar";
 import Otp from '../registration/otp'
 import Login from '../Auth/login';
@@ -28,7 +28,7 @@ import AddBook from '../admin/books/add-book';
 import CreateAuthor from '../admin/authors/create-author';
 import EditAuthor from '../admin/authors/edit-author';
 import Users from '../admin/users/users';
-import {  useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import Authors from '../user/authors';
 import Books from '../user/books';
@@ -40,137 +40,162 @@ import Profile from '../user/profile';
 
 
 
-export const  AppLayout = ()=>{
+export const AppLayout = () => {
 
-  const naviagte =useNavigate()
-  const loggedInUser = useSelector((state) => state.user.value);
-  const [anchorElUser, setAnchorElUser] = useState(null)
+    const naviagte = useNavigate()
+    const loggedInUser = useSelector((state) => state.user.value);
+    const [anchorElUser, setAnchorElUser] = useState(null)
 
 
-  
 
-    
+
+
     const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget)
-  }
+        setAnchorElUser(event.currentTarget)
+    }
 
-  const handleCloseUserMenu = () => {
-      setAnchorElUser(null)
-  }
-const handleUpload=()=>{
-    console.log('Upload')
-}
-  const onProfileImageChange = (e) => {
-    let file = e.target.files[0];
-    let formData = new FormData();
-    formData.append("file", file);
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null)
+    }
+    const handleUpload = () => {
+        console.log('Upload')
+    }
+    const onProfileImageChange = (e) => {
+        let file = e.target.files[0];
+        let formData = new FormData();
+        formData.append("file", file);
 
-  
-};
+
+    };
 
 
     const handleLogout = () => {
-      window.localStorage.removeItem("token");
-      naviagte('/')
-      handleCloseUserMenu()
-  }
+        window.localStorage.removeItem("token");
+        naviagte('/')
+        handleCloseUserMenu()
+    }
 
 
 
 
-return(
+    return (
 
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Library Managment System
-          </Typography>
-          <Box
-                            sx={{
-                                flexGrow: 0,
-                            }}
-                        >
-                            {loggedInUser ? (
-                                <>
-                                    <Tooltip title="Open settings">
-                                        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                            <Avatar> {loggedInUser.name} </Avatar>
-                                        </IconButton>
-                                    </Tooltip>
-                                    <Menu
-                                        sx={{ mt: "45px" }}
-                                        id="menu-appbar"
-                                        anchorEl={anchorElUser}
-                                        anchorOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        open={Boolean(anchorElUser)}
-                                        onClose={handleCloseUserMenu}
-                                    >
-                                        <MenuItem onClick={handleCloseUserMenu}>
-                                            <Typography textAlign="center">Dashboard</Typography>
-                                        </MenuItem>
-                                      
-                                        <MenuItem onClick={handleLogout}>
-                                            <Typography textAlign="center">Logout</Typography>
-                                        </MenuItem>
-                                    </Menu>
-                                </>
-                            ) : (
-                                <Button>
-                                    Login
-                                </Button>
-                            )}
-                        </Box>
-         
-        </Toolbar>
-      </AppBar>
-      <Routes>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Library Managment System
+                    </Typography>
+                    <Box
+                        sx={{
+                            flexGrow: 0,
+                        }}
+                    >
+                        {loggedInUser ? (
+                            <>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar> {loggedInUser.name} </Avatar>
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: "45px" }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    <MenuItem onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center">Dashboard</Typography>
+                                    </MenuItem>
+
+                                    <MenuItem onClick={handleLogout}>
+                                        <Typography textAlign="center">Logout</Typography>
+                                    </MenuItem>
+                                </Menu>
+                            </>
+                        ) : (
+                            <Button>
+                                Login
+                            </Button>
+                        )}
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+            <Routes>
+                {
+                    loggedInUser && (
+                        <>
+                            <Route path="/" element={<Login />} />
+                            {
+                                loggedInUser.role === "admin" && (
+                                    <>
+                                        <Route path="/" element={<Login />} />
+                                        <Route path="/admin/dashboard/book/edit/:id" exact element={<EditBook />} />
+                                        <Route path="/admin/dashboard" exact element={<Sidebar />} />
+                                        <Route path="/admin/dashboard/author/edit/:id" exact element={<EditAuthor />} />
+                                        <Route path="/admin/dashboard/books" exact element={<Book />} />
+                                        <Route path="/admin/dashboard/orders" exact element={<Orders />} />
+                                        <Route path="/admin/dashboard/authors" exact element={<Author />} />
+                                        <Route path="/admin/dashboard/book/create" exact element={<AddBook />} />
+                                        <Route path="/admin/dashboard/users" exact element={<Users />} />
+                                        <Route path="/admin/dashboard/author/create" exact element={<CreateAuthor />} />
+                                        <Route path="/" exact element={<Login />} />
+                                    </>
+
+                                )
+                            }
+                        </>
+
+                    )}
+                {loggedInUser.role === "user" && (
+                    <>
+                    <Route path="/user/dashboard" exact element={<Sidebar />} />
+                <Route path="/user/authors" exact element={<Authors />} />
+                <Route path="/user/books" exact element={<Books />} />
+                <Route path="/user/placeorder" exact element={<PlaceOrder />} />
+                <Route path="/user/order" exact element={<Order />} />
+                <Route path="/user/profile" exact element={<Profile />} />
+                <Route path="/register" exact element={<SignUp />} />
+                <Route path="/registration/otp/:id" exact element={<Otp />} />
+                <Route path="/" exact element={<Login />} />
+                    </>
+                )}
+
+                
                
-                <Route path = "/admin/dashboard" exact element={<Sidebar/>} />
-                <Route path = "/user/dashboard" exact element={<Sidebar/>} />
-                <Route path = "/user/authors" exact element={<Authors/>} />
-                <Route path = "/user/books" exact element={<Books/>} />
-                 <Route path = "/user/placeorder" exact element={<PlaceOrder/>} />
-                 <Route path = "/user/order" exact element={<Order/>} />
-                 <Route path = "/user/profile" exact element={<Profile/>} />
 
-                <Route path = "/register" exact element={<SignUp/>} />
-                <Route path = "/registration/otp/:id" exact element={<Otp/>} />
-                <Route path = "/admin/dashboard/book/edit/:id" exact element={<EditBook/>} />
-                <Route path = "/admin/dashboard/author/edit/:id" exact element={<EditAuthor/>} />
-                <Route path = "/admin/dashboard/books" exact element={<Book/>} />
-                <Route path = "/admin/dashboard/orders" exact element={<Orders/>} />
-                <Route path = "/admin/dashboard/authors" exact element={<Author/>} />
-                <Route path = "/admin/dashboard/book/create" exact element={<AddBook/>} />
-                <Route path = "/admin/dashboard/users" exact element={<Users/>} />
-                <Route path = "/admin/dashboard/author/create" exact element={<CreateAuthor/>} />
-                <Route path = "/" exact element={<Login/>} />
+               
 
-            
+               
+
+
 
             </Routes>
-           
-    </Box>
 
-    
-)
+        </Box>
+
+
+    )
 
 
 
